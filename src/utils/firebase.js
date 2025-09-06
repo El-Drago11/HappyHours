f4186756cdc9be92be3871509e99cd43155ed45c
Rahul Singh Rawat <111895587+El-Drago11@users.noreply.github.com>
2025-09-06 11:05:50 +0530

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,19 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxwZ1vWFRrEJY4Ck4GzvzdVC5G3wg14r8",
-  authDomain: "happyhours-77fd1.firebaseapp.com",
-  projectId: "happyhours-77fd1",
-  storageBucket: "happyhours-77fd1.appspot.com",
-  messagingSenderId: "1020906000677",
-  appId: "1:1020906000677:web:108f350a263136f3560acc",
-  measurementId: "G-9RKMXEYE17"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
